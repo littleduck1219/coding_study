529e969b57d5a6c89ecf3422cd49a3aefc4b1580
gyeong deok <littleduck1219@gmail.com>
2024-02-10 17:23:56 +0900

    java varialbe

---
+++ Language/Java/java-start/src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.print("hello world1");
+        System.out.print("hello world2");
+        System.out.print("hello world3");
+        System.out.print("hello world4");
+    }
+}
---
+++ Language/Java/java_start/src/Hello2.java
@@ -0,0 +1,8 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World1");
+        System.out.println("Hello World1");
+        System.out.println("Hello World1");
+    }
+}
