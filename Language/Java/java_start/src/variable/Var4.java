229b51c84d0cc36c844bc39c6a1c9a9a7d04f675
gyeong deok <littleduck1219@gmail.com>
2024-02-10 17:32:12 +0900

    java set variable

---
+++ Language/Java/java_start/src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        int b = 2;
+        System.out.println(b);
+    }
+}
