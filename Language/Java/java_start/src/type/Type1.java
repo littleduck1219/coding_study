8d4d8a1357dcf8d7cb92d99aa97787a8adb25663
gyeong deok <littleduck1219@gmail.com>
2024-02-11 23:18:38 +0900

    var q1

---
+++ Language/Java/java_start/src/type/Type1.java
@@ -0,0 +1,18 @@
+package type;
+
+public class Type1 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //참,거짓
+        char d = 'a'; //문자
+        String e = "hello java"; //문자열
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
---
+++ Language/Java/java_start/src/type/Type2.java
@@ -0,0 +1,13 @@
+package type;
+
+public class Type2 {
+    public static void main(String[] args) {
+        byte b = 127; // -128 ~ 12
+        short s = 32767; // -32768 ~ 32.767
+        int i = 2147483647; // -2147483648 ~ 2147483647
+        long l = 92233720;
+
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
