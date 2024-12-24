c9b5b3e81e8245e26956801c42ae2fe14831659b
littleduck <107936957+littleduck1219@users.noreply.github.com>
2024-12-24 14:46:17 +0900

    java method

--- Language/Java/java-start/src/HelloJava2.java
+++
@@ -1,9 +0,0 @@
-public class HelloJava2 {
-
-    public static void main(String[] args) {
-        System.out.print("hello world1");
-        System.out.print("hello world2");
-        System.out.print("hello world3");
-        System.out.print("hello world4");
-    }
-}
--- Language/Java/java-start/src/variable/Var2.java
+++
@@ -1,9 +0,0 @@
-package variable;
-
-public class Var2 {
-    public static void main(String[] args) {
-        int a = 10;
-
-        System.out.println(a);
-    }
-}
