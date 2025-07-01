912df6bb6268e03a708a5829b0055bb831b535cf
littleduck <littleduck1219@gmail.com>
2025-07-01 22:50:01 +0900

    null

---
+++ Language/Java/java_basic/src/ref/Method1.java
@@ -0,0 +1,14 @@
+package ref;
+
+public class Method1 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 10;
+        student1.grade = 1;
+
+        Student student2 = new Student();
+        student2.name = "학생2";
+        student2.age = 20;
+    }
+}
