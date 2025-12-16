435d1a8fdc37d77afd883ffcbbf4613349686540
littleduck <107936957+littleduck1219@users.noreply.github.com>
2025-12-16 16:58:03 +0900

    spring

---
+++ Frameworks/spring/demo/src/test/java/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,54 @@
+package repository;
+
+import littlduck.demo.domain.Member;
+import littlduck.demo.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("duck");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        Assertions.assertEquals(member, result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("duck");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("duck2");
+        repository.save(member2);
+
+        Member result = repository.findByName("duck").get();
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("duck");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("duck1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
